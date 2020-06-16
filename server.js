ad9511f2a3b68169b9428644ab49cd5b6239b702
Akash Tyagi <tyagiakash630@gmail.com>
2020-06-16 08:08:18 +0530

    Added fs and path module

--- server.js
+++ server.js
@@ -1,11 +1,52 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
 const hostname = 'localhost';
 const port = 3000;
 const server = http.createServer((req,res)=>{
-   console.log(req.headers);
-   res.statusCode = 200;
-   res.setHeader('content-type','text-html');
-   res.end('<html><body><h1>Hello</h1></body></html>');
+   console.log("Request for "+ req.url + " by method "+req.method);
+   if(req.method == 'GET'){
+       let fileUrl;
+       if(req.url == '/'){
+            fileUrl = '/index.html';
+            
+       }
+        else
+            fileUrl = req.url;
+
+        let filePath = path.resolve('./public'+fileUrl);
+        const fileExt = path.extname(filePath);
+        if(fileExt == '.html'){
+            fs.exists(filePath,(exists)=>{
+               if(!exists){
+                res.statusCode = 404;
+                res.setHeader('content-type','text-html');
+                res.end('<html><body><h1>Error 404</h1></body></html>');
+                return;
+               }
+               
+               res.statusCode = 200;
+               res.setHeader('content-type','text-html');
+               fs.createReadStream(filePath).pipe(res);
+               res.end('<html><body><h1></h1></body></html>');
+              
+            });
+        }
+        else{
+        res.statusCode = 404;
+        res.setHeader('content-type','text-html');
+        res.end('<html><body><h1>Error 404</h1></body></html>');
+        return;
+        }
+   
+    }
+    else{
+        res.statusCode = 404;
+        res.setHeader('content-type','text-html');
+        res.end('<html><body><h1>Error 404</h1></body></html>');
+        return; 
+    }
 
 });
 
